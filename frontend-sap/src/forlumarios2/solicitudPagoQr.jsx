a0fe8c58440508f051778338d1f00ebbe14fa134
cesar <cesarcrac44@gmail.com>
2023-06-08 21:50:35 -0400

    boleta qr

---
+++ frontend-sap/src/forlumarios2/solicitudPagoQr.jsx
@@ -0,0 +1,141 @@
+import React, { useState, useEffect } from 'react';
+import { Form, Button, Modal, Col, Row } from 'react-bootstrap';
+import axios from 'axios';
+
+function SolicitudPagoQr() {
+  const [boletas, setBoletas] = useState([]);
+  const [aceptados, setAceptados] = useState([]);
+  const [rechazados, setRechazados] = useState([]);
+  const [nombres, setNombres] = useState({});
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/boletas')
+      .then(response => {
+        setBoletas(response.data.filter(boleta => boleta.estado === 4 && boleta.id_user !== null));
+        setAceptados(response.data.filter(boleta => boleta.estado === 5 && boleta.id_user !== null));
+        setRechazados(response.data.filter(boleta => boleta.estado === 6 && boleta.id_user !== null));
+        
+        response.data.forEach(async boleta => {
+          const userName = await getUserName(boleta.id_user);
+          setNombres(nombres => ({...nombres, [boleta.id]: userName}));
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  const getUserName = async (userId) => {
+    try {
+      const response = await axios.get("http://localhost:8000/api/users");
+      const user = response.data.find(user => user.id === userId);
+      if (user) {
+        return user.name;
+      } else {
+        return "Usuario no encontrado";
+      }
+    } catch (error) {
+      console.error(error);
+      return "";
+    }
+  };
+
+  const handleAceptadoClick = (id) => {
+    const boletaAceptada = boletas.find(boleta => boleta.id === id);
+
+    axios.put(`http://localhost:8000/api/boletas/${id}`, { estado: 5 })
+      .then(response => {
+        setAceptados([...aceptados, boletaAceptada]);
+        setBoletas(boletas.filter(boleta => boleta.id !== id));
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+  
+  const handleRechazoClick = (id) => {
+    const boletaRechazada = boletas.find(boleta => boleta.id === id);
+    axios.put(`http://localhost:8000/api/boletas/${id}`, { estado: 6 })
+      .then(response => {
+        setRechazados([...rechazados, boletaRechazada]);
+        setBoletas(boletas.filter(boleta => boleta.id !== id));
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <Row style={{marginLeft: "0px", background: "#0C4D51"}}>
+              <h2 style={{marginLeft: "520px", color: "#F1F1F2"}}>Solicitudes de Pago por QR:</h2>
+              <></>
+      <Col style={{marginLeft: "5px", background: "#F3F3FA"}}>
+        <h2>Pendientes:</h2>
+        <ul>
+        <p>--------------------------------------------------</p>
+
+          {boletas.map(boleta => (
+            <li key={boleta.id}>
+             
+              {/* <p>id usuario: {boleta.id_user}</p> */}
+              <p>Nombre de usuario: {nombres[boleta.id]}</p>
+
+              <p>Numero de transaccion: {boleta.nro_transaccion}</p>
+              <p>Meses a pagar : {boleta.mensualidad}</p>
+              <p>Monto a pagar: {boleta.monto}</p>
+              <p>Fecha de deposito: {boleta.fecha_deposito}</p>
+
+              <Button style={{backgroundColor: "#198754", marginLeft: "35px"}} onClick={() => handleAceptadoClick(boleta.id)}>Aceptar</Button>
+              <Button style={{backgroundColor: "#DC3545", marginLeft: "40px"}} onClick={() => handleRechazoClick(boleta.id)}>Rechazar</Button>
+              <p>--------------------------------------------------</p>
+            </li>
+          ))}
+        </ul>
+      </Col> 
+        
+      <Col style={{marginLeft: "5px", background: "#F3F3FA"}}>
+        <h2>Aceptados:</h2>
+        <p>--------------------------------------------------</p>
+
+        <ul>
+          {aceptados.map(boleta => (
+            <li key={boleta.id}>
+             
+              {/* <p>id usuario: {boleta.id_user}</p> */}
+              <p>Nombre de usuario: {nombres[boleta.id]}</p>
+              <p>Numero de transaccion: {boleta.nro_transaccion}</p>
+              <p>Meses a pagar : {boleta.mensualidad}</p>
+              <p>Monto a pagar: {boleta.monto}</p>
+              <p>Fecha de deposito: {boleta.fecha_deposito}</p>
+              <p>--------------------------------------------------</p>
+            </li>
+          ))}
+        </ul>
+      </Col>
+
+        <Col style={{marginLeft: "5px", background: "#F3F3FA"}}>
+          <h2>Rechazados:</h2>
+          <p>--------------------------------------------------</p>
+
+          <ul>
+          {rechazados.map(boleta => (
+            <li key={boleta.id}>
+          
+              {/* <p>id usuario: {boleta.id_user}</p> */}
+              <p>Nombre de usuario: {nombres[boleta.id]}</p>
+
+              <p>Numero de transaccion: {boleta.nro_transaccion}</p>
+              <p>Meses a pagar : {boleta.mensualidad}</p>
+              <p>Monto a pagar: {boleta.monto}</p>
+              <p>Fecha de deposito: {boleta.fecha_deposito}</p>
+              <p>--------------------------------------------------</p>
+            </li>
+          ))}
+        </ul>
+         
+        </Col>
+      </Row>
+    );
+  }
+
+export default SolicitudPagoQr;
